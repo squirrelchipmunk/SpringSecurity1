5a1cb6ad760bae1b26cda63b4d04a496bd2261c7
이원준 <ggoong8@gmail.com>
2022-06-08 23:47:55 +0900

    페이스북 로그인

--- src/main/java/com/exam/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/exam/security1/config/oauth/PrincipalOauth2UserService.java
@@ -8,6 +8,9 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import com.exam.security1.auth.PrincipalDetails;
+import com.exam.security1.config.oauth.provider.FacebookUserInfo;
+import com.exam.security1.config.oauth.provider.GoogleUserInfo;
+import com.exam.security1.config.oauth.provider.OAuth2UserInfo;
 import com.exam.security1.model.User;
 import com.exam.security1.repository.UserRepository;
 
@@ -33,11 +36,24 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 		// userRequest -> loadUser() -> 회원 프로필
 		System.out.println("userRequest : "+oauth2User.getAttributes());
 		
-		String provider = userRequest.getClientRegistration().getClientId(); // google, facebook 등
-		String providerId = oauth2User.getAttribute("sub");
+		OAuth2UserInfo oAuth2UserInfo = null;
+		if(userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+			System.out.println("구글 로그인 요청");
+			oAuth2UserInfo = new GoogleUserInfo(oauth2User.getAttributes());
+		}
+		else if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")){
+			System.out.println("페이스북 로그인 요청");
+			oAuth2UserInfo = new FacebookUserInfo(oauth2User.getAttributes());
+		}
+		else {
+			System.out.println("구글과 페이스북만 지원합니다.");
+		}
+		
+		String provider = oAuth2UserInfo.getProvider();
+		String providerId = oAuth2UserInfo.getProviderId();
 		String username = provider+"_"+providerId;
 		String password = bCryptPasswordEncoder.encode("비밀번호");
-		String email = oauth2User.getAttribute("email");
+		String email = oAuth2UserInfo.getEmail();
 		String role = "ROLE_USER";
 		
 		User userEntity = userRepository.findByUsername(username);
